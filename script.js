a3ed42cd94b0df866a33b104c58166975bc2cb4b
jorge niglia <jorge.niglia@gmail.com>
2023-01-06 22:26:29 -0300

    up to manipulacion del DOM

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const btnOnClick = (event) => {
+  console.log({ event });
+  event.preventDefault();
+  const suma = Number(input1.value) + Number(input2.value);
+  pResult.innerText = `Resultado = ${suma}`;
+};
+
 const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const button = document.querySelector("#btnCalcular");
 const pResult = document.querySelector("#result");
-let counter = 0;
-const btnOnClick = () => {
-  const suma = Number(input1.value) + Number(input2.value);
-  pResult.innerHTML = `Resultado = ${suma}`;
-};
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", btnOnClick);
